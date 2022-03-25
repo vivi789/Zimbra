91c8829a7ab9554f25f560a32a3489798c8371a3
vivi789 <46598633+vivi789@users.noreply.github.com>
2022-03-25 09:51:11 +0700

    Add files via upload

---
+++ relay.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+zmhostname=`hostname`
+read -p "smtp_server: " srv
+read -p "port: " port
+read -p "user: " user
+read -p "password: " pass
+#echo $srv $user:$pass > /opt/zimbra/conf/relay_password
+#su - zimbra -c "postmap /opt/zimbra/conf/relay_password"
+#su - zimbra -c "postmap -q $srv /opt/zimbra/conf/relay_password"
+#su - zimbra -c "zmprov ms $zmhostname zimbraMtaSmtpSaslPasswordMaps lmdb:/opt/zimbra/conf/relay_password"
+#su - zimbra -c "zmprov ms $zmhostname zimbraMtaSmtpSaslAuthEnable yes"
+#su - zimbra -c "zmprov ms $zmhostname zimbraMtaSmtpCnameOverridesServername no"
+#su - zimbra -c "zmprov ms $zmhostname zimbraMtaSmtpTlsSecurityLevel may"
+#su - zimbra -c "zmprov ms $zmhostname zimbraMtaSmtpSaslSecurityOptions noanonymous"
+#su - zimbra -c "zmprov ms $zmhostname zimbraMtaRelayHost $srv:$port"
+#su - zimbra -c "postfix reload"
+echo "Relay configuration was successful!!!"

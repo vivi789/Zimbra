90663199e8f67549ad3fc831aa7fa309fe24c973
vivi789 <46598633+vivi789@users.noreply.github.com>
2022-04-22 09:10:38 +0700

    Create disable_relay.sh

---
+++ disable_relay.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+su - zimbra -c 'zmprov ms `zmhostname` zimbraMtaRelayHost ""'
+su - zimbra -c 'zmprov ms `zmhostname` zimbraMtaFallbackRelayHost ""'
+
+a=$(su - zimbra -c 'postconf -n | grep -E "relayhost" | cut -d "=" -f 2')
+b=$(su - zimbra -c 'postconf -n | grep -E "smtp_fallback_relay" | cut -d "=" -f 2')
+
+sleep 30
+
+if [ -z $a ] && [ -z $b ]
+then
+        echo "disable relay successfully"
+        su - zimbra -c 'postfix reload'
+else
+        echo "disable relay failed"
+        su - zimbra -c 'postconf -n | grep "relay"'
+fi

6a5349f3a51b699a9f91e662f7543fe3b273b13d
vivi789 <46598633+vivi789@users.noreply.github.com>
2022-04-27 10:37:23 +0700

    Update disable_relay.sh

--- disable_relay.sh
+++ disable_relay.sh
@@ -5,7 +5,7 @@ su - zimbra -c 'zmprov ms `zmhostname` zimbraMtaFallbackRelayHost ""'
 a=$(su - zimbra -c 'postconf -n | grep -E "relayhost" | cut -d "=" -f 2')
 b=$(su - zimbra -c 'postconf -n | grep -E "smtp_fallback_relay" | cut -d "=" -f 2')
 
-sleep 30
+sleep 60
 
 if [ -z $a ] && [ -z $b ]
 then
